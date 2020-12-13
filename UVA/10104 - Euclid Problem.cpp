9caa42cf4cbb473d48c1339fbeb6764d03e4e8c0
Ahnaf Chowdhury <57319611+Ahnaf-41M@users.noreply.github.com>
2020-12-13 19:44:59 +0600

    Files

--- UVA/10104 - Euclid Problem.cpp
+++ UVA/10104 - Euclid Problem.cpp
@@ -1,23 +1,54 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#include <ext/rope>
+
+#define  pb   push_back
+#define  int  long long
+#define  endl "\n"
+#define  MX   100005
+
+#define  W(t)            while(t--)
+#define  all(v)          v.begin(),v.end()
+#define  gcd(a,b)        __gcd(a,b)
+#define  lcm(a,b)        (a*b)/gcd(a,b)
+#define  rep(i,a,b)      for(int i = a; i <= b; i++)
+#define  irep(i,a,b)     for(int i = b; i >= a; i--)
+
 using namespace std;
+using namespace __gnu_cxx;
 
-int gcd(int A, int B)
+int Extended_gcd(int a, int b, int &x, int &y)
 {
-    if(A==0)
-        return B;
+   if (b == 0) {
+      x = 1;
+      y = 0;
+      return a;
+   }
+   int x1, y1;
+   int d = Extended_gcd(b, a % b, x1, y1);
 
-    return gcd(B%A,A);
+   x = y1;
+   y = x1 - y1 * (a / b);
+
+   return d;
 }
-
-int main()
+void solve()
 {
-    int A,B,X,Y,D;
+   int a, b;
 
-    while(cin>>A>>B)
-    {
-        D = gcd(A,B);
-        X = (D-B)/A;
+   while (cin >> a >> b)
+   {
+      int x, y, d;
+      d = Extended_gcd(a, b, x, y);
+      cout << x << " " << y << " " << d << endl;
+   }
 
-        cout<<X<<" "<<1<<" "<<D<<endl;
-    }
+   return;
+}
+signed main()
+{
+   ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+   solve();
+
+   return 0;
 }
