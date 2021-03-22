204d132c28ddafcf0601b18495977a5544274792
Ahnaf Chowdhury <57319611+Ahnaf-41M@users.noreply.github.com>
2021-03-22 09:23:02 +0600

    Files

--- LightOJ/1090 - Trailing Zeroes (II).cpp
+++ LightOJ/1090 - Trailing Zeroes (II).cpp
@@ -1,79 +1,63 @@
-#include <bits/stdc++.h>
-#include <ext/rope>
-#include <ext/pb_ds/tree_policy.hpp>
-#include <ext/pb_ds/assoc_container.hpp>
-
+#include<bits/stdc++.h>
+#define  MX      1000005
 #define  ff      first
 #define  ss      second
 #define  pb      push_back
-#define  ll      long long
-#define  MX      1000005
-#define  inf     1000000
-#define  mod     1000000007
-#define  endl    "\n"
-
-#define  W(t)            while(t--)
-#define  gcd(a,b)        __gcd(a,b)
-#define  lcm(a,b)        (a*(b/gcd(a,b)))
-#define  all(v)          v.begin(),v.end()
-#define  ZERO(a)         memset(a,0,sizeof(a))
-#define  MINUS(a)        memset(a,-1,sizeof(a))
-#define  rep(i,a,b)      for(i = a; i < b; i++)
-#define  IOS             ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define  int     long long
+#define  PII     pair<int,int>
+#define  all(v)  v.begin(),v.end()
 using namespace std;
-using namespace __gnu_cxx;
-using namespace __gnu_pbds;
-template <typename T>  using ordered_set =
-   tree<T, null_type, less<T>,
-   rb_tree_tag, tree_order_statistics_node_update>;
 
-ll cnt_2, cnt_5;
 
 struct info
 {
-   ll two, five, zero;
-} fact[MX];
+   int two, five;
+} fact[MX + 5];
 
-inline void prime_fact(ll N)
+int Fun1(int x)
 {
-   while ( N % 2 == 0 )
-   {
-      N /= 2;
-      cnt_2++;
-   }
-   while ( N % 5 == 0 )
-   {
-      N /= 5;
-      cnt_5++;
+   int cnt = 0;
+   while (x % 2 == 0)
+      x /= 2, cnt++;
+   return cnt;
+}
+int Fun2(int x)
+{
+   int cnt = 0;
+   while (x % 5 == 0)
+      x /= 5, cnt++;
+   return cnt;
+}
+void Pre_Cal()
+{
+   for (int i = 2; i <= MX; i++) {
+      fact[i].two = fact[i - 1].two + Fun1(i); //number of 2s in the factorization of fact[i]
+      fact[i].five = fact[i - 1].five + Fun2(i); //number of 5s in the factorization of fact[i]
    }
 }
-inline void pre_process()
-{
-   for (ll i = 1; i <= 1000000; i++) {
-      if (i % 2 == 0 || i % 5 == 0)
-         prime_fact(i);
-      fact[i].zero = min(cnt_5, cnt_2);
-      fact[i].five = cnt_5;
-      fact[i].two  = cnt_2;
-   }
-}
-int main()
-{
-   pre_process();
 
-   ll t, n, r, p, q;
-   scanf("%lld", &t);
+int n, r, p, q;
+signed main()
+{
+   ios::sync_with_stdio(false);
+   cin.tie(NULL);
+   cout.tie(NULL);
+
+   Pre_Cal();
+
+   int t;
+   cin >> t;
 
    for (int k = 1; k <= t; k++) {
-      scanf("%lld%lld%lld%lld", &n, &r, &p, &q);
-      cnt_2 = 0, cnt_5 = 0;
-      prime_fact(p);
-      
-      ll x = fact[n].five - fact[n - r].five - fact[r].five;
-      ll y = fact[n].two - fact[n - r].two - fact[r].two;
-      ll ans = min(x + cnt_5 * q, y + cnt_2 * q);
+      cin >> n >> r >> p >> q;
 
-      printf("Case %d: %lld\n", k, ans);
+      int p_2 = Fun1(p) * q, p_5 = Fun2(p) * q;
+      int d1 = fact[n].two - (fact[n - r].two + fact[r].two);
+      int d2 = fact[n].five - (fact[n - r].five + fact[r].five);
+
+      int ans = min(d1 + p_2, d2 + p_5);
+      cout << "Case " << k << ": " << ans << endl;
+
    }
-
+   return 0;
 }
