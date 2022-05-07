550802a4b8b5b966a6917f18e78a6d63ea514f33
Ahnaf-41M <mdahnaf7@gmail.com>
2022-05-07 12:20:37 +0600

    map

---
+++ Spoj/SBANK - Sorting Bank Accounts.cpp
@@ -0,0 +1,43 @@
+#include "bits/stdc++.h"
+#define  ff           first
+#define  ss           second
+#define  MX           100005
+#define  pb           push_back
+#define  int          long long
+#define  PII          pair<string, int>
+#define  endl         "\n"
+#define  all(v)       v.begin(),v.end()
+#define  rep(i,a,b)   for(int i = a; i <= b; i++)
+#define  irep(i,b,a)  for(int i = b; i >= a; i--)
+using namespace std;
+
+void Solve(int tc)
+{
+    int n;
+    map<string, int> mp;
+
+    cin >> n;
+    cin.ignore();
+    while (n--) {
+        string s = "";
+        getline(cin, s);
+        mp[s]++;
+    }
+
+    for (auto it : mp) cout << it.ff << it.ss << "\n";
+    cout << "\n";
+}
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T = 1;
+    cin >> T;
+
+    for (int tc = 1; tc <= T; tc++) {
+        Solve(tc);
+    }
+
+    return 0;
+}
