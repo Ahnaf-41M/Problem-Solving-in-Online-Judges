ea6059aa375de12a2e6a7267bbcc1c2e26fc6882
Ahnaf-41M <mdahnaf7@gmail.com>
2023-10-27 17:29:10 +0600

    adhoc-bit

---
+++ Spoj/GOO - Game Of Ones.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+#define  ff         first
+#define  ss         second
+#define  MX         100005
+#define  pb         push_back
+#define  int        long long
+#define  PII        pair<int, int>
+#define  all(v)     v.begin(),v.end()
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T>  using ordered_set =
+    tree<T, null_type, less<T>, //less_equal for multiset
+    rb_tree_tag, tree_order_statistics_node_update>;
+
+void Solve(int tc)
+{
+    int n;
+    cin >> n;
+
+    int tot_numbers = (1LL << (n - 1));
+    int tot_ones = tot_numbers + (tot_numbers * (n - 1) / 2);
+    cout << tot_numbers << " " << tot_ones << "\n";
+}
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T = 1;
+    cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        Solve(tc);
+    }
+
+}
