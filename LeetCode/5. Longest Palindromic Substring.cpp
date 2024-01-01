9d61f072e28ec7fa63039824413191de5b985388
Ahnaf-41M <mdahnaf7@gmail.com>
2024-01-01 20:35:36 +0600

    update[200~5. Longest Palindromic Substring ; ; ";

--- LeetCode/5. Longest Palindromic Substring.cpp
+++ LeetCode/5. Longest Palindromic Substring.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
+    int dp[1001][1001];
+    string str;
+
+    bool func(int l, int r) {
+        if (l >= r)
+            return 1;
+        if (dp[l][r] != -1)
+            return dp[l][r];
+
+        if (str[l] == str[r])
+            return dp[l][r] = func(l + 1, r - 1);
+        else
+            return dp[l][r] = 0;
+    }
     string longestPalindrome(string s) {
+        memset(dp, -1, sizeof(dp));
+        str = s;
         string ans = "";
         int n = s.size();
         int ansLen = 1;
         pair<int, int> ansPair = {0, 0};
 
         for (int i = 0; i < n; i++) {
-            int l = i, r = i;
-            // for odd length
-            while (l >= 0 && r < n && s[l] == s[r]) {
-                if (r - l + 1 > ansLen) {
-                    ansLen = r - l + 1;
-                    ansPair = {l, r};
+            for (int j = 0; j < n; j++) {
+                if (func(i, j) && ansLen < j - i + 1) {
+                    ansLen = j - i + 1;
+                    ansPair = {i, j};
                 }
-                l--, r++;
-            }
-
-            // for even length
-            l = i - 1, r = i;
-            while (l >= 0 && r < n && s[l] == s[r]) {
-                if (r - l + 1 > ansLen) {
-                    ansLen = r - l + 1;
-                    ansPair = {l, r};
-                }
-                l--, r++;
             }
         }
-        //cout << ansPair.first << " " << ansPair.second<<"\n";
+        // cout << ansPair.first << " " << ansPair.second<<"\n";
         for (int i = ansPair.first; i <= ansPair.second; i++) {
             ans.push_back(s[i]);
         }
