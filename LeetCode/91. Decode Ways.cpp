7767d8e54ab310c9749ee7fee6a9c4f32826b3be
Ahnaf-41M <mdahnaf7@gmail.com>
2024-01-04 11:38:06 +0600

    update-dp-string

--- LeetCode/91. Decode Ways.cpp
+++ LeetCode/91. Decode Ways.cpp
@@ -2,32 +2,60 @@ class Solution {
 public:
     string str;
     int n;
-    map<string, int> dp;
-    map<string, char> mp;
-
-    int call(string s) {
-        if (s.empty())
+    int dp[101];
+    int call(int pos) {
+        if (pos >= n)
             return 1;
-        if (dp.count(s))
-            return dp[s];
+        if (str[pos] == '0')
+            return 0;
+        if (dp[pos] != -1)
+            return dp[pos];
 
-        int res = 0;
-        string cur;
-        cur.push_back(s[0]);
-        if (mp.count(cur))
-            res += call(s.substr(1));
-        if (s.size() > 1) {
-            cur.push_back(s[1]);
-            if (mp.count(cur))
-                res += call(s.substr(2));
+        int res = call(pos + 1);
+        if (pos + 1 < n &&
+                (str[pos] == '1' || (str[pos] == '2' && str[pos + 1] <= '6'))) {
+            res += call(pos + 2);
         }
-        return dp[s] = res;
+
+        return dp[pos] = res;
     }
     int numDecodings(string s) {
+        memset(dp, -1, sizeof(dp));
         n = s.size();
-        for (int i = 1; i <= 26; i++) {
-            mp[to_string(i)] = 65 + i - 1;
-        }
-        return call(s);
+        str = s;
+        return call(0);
     }
 };
+// class Solution {
+// public:
+//     string str;
+//     int n;
+//     map<string, int> dp;
+//     map<string, char> mp;
+
+//     int call(string s) {
+//         if (s.empty())
+//             return 1;
+//         if (dp.count(s))
+//             return dp[s];
+
+//         int res = 0;
+//         string cur;
+//         cur.push_back(s[0]);
+//         if (mp.count(cur))
+//             res += call(s.substr(1));
+//         if (s.size() > 1) {
+//             cur.push_back(s[1]);
+//             if (mp.count(cur))
+//                 res += call(s.substr(2));
+//         }
+//         return dp[s] = res;
+//     }
+//     int numDecodings(string s) {
+//         n = s.size();
+//         for (int i = 1; i <= 26; i++) {
+//             mp[to_string(i)] = 65 + i - 1;
+//         }
+//         return call(s);
+//     }
+// };
