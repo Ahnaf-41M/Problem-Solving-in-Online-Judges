462cda7857d378b1a9becf67423ea8dfa3b85668
Ahnaf-41M <mdahnaf7@gmail.com>
2024-05-01 18:11:18 +0600

    HashMap

---
+++ LeetCode/2352. Equal Row and Column Pairs.java
@@ -0,0 +1,23 @@
+class Solution {
+
+    public int equalPairs(int[][] grid) {
+        int n = grid.length, ans = 0;
+        Map<Integer, Integer> mp = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            int hashVal = 0;
+            for (int j = 0; j < n; j++) {
+                hashVal = grid[i][j] + 5 * hashVal;
+            }
+            mp.put(hashVal, mp.getOrDefault(hashVal, 0) + 1);
+        }
+        for (int j = 0; j < n; j++) {
+            int hashVal = 0;
+            for (int i = 0; i < n; i++) {
+                hashVal = grid[i][j] + 5 * hashVal;
+            }
+            ans += mp.getOrDefault(hashVal, 0);
+        }
+        return ans;
+    }
+}
